690c5e91da0117216035a472fab446877de8056c
Tanya Vasylovych <tetiana.vasylovych.work@gmail.com>
2025-08-12 21:33:59 +0300

    upd vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,6 +7,5 @@ export default defineConfig({
   build: {
     sourcemap: true,
   },
+  base: '/03-react-movies',
 });
-
-
