d3c0bfb9a1940b0482072c329c60164d90608a68
Tanya Vasylovych <tetiana.vasylovych.work@gmail.com>
2025-08-12 20:53:38 +0300

    upd

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/App/App";
+import ReactDOM from 'react-dom/client';
+import App from './components/App/App';
+import React from 'react';
 
 // Нормалізація стилів
-import "modern-normalize";
+import 'modern-normalize';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
